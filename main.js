fcb3886c9bb2cb2dc2610508b257988417057cc3
578025tumi <138103070+578025tumi@users.noreply.github.com>
2026-05-16 04:08:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
       transmission: '8-Speed Dual-Clutch',
       color: 'Beluga Black',
       interior: 'Linen / Beluga',
-      image: '6CJUWC7VEBI4ZIYXTUQF4TQ46E',
+      image: 'https://unsplash.com/photos/blue-coupe-on-road-cAbH8B-14Vo',
       badge: 'Low Miles'
     },
     {
